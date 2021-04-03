8b80c75309c85d20a533eb8b5cc21e9130b0312b
LEESANGNAM <fgh0116@naver.com>
2021-04-03 13:19:27 +0900

    클럽CRUD완성

--- src/main/java/kr/ac/yeonsung/demo/controller/ClubController.java
+++ src/main/java/kr/ac/yeonsung/demo/controller/ClubController.java
@@ -1,18 +1,14 @@
 package kr.ac.yeonsung.demo.controller;
 
 
-import kr.ac.yeonsung.demo.domain.Join;
-import kr.ac.yeonsung.demo.domain.JoinClub;
 import kr.ac.yeonsung.demo.domain.club.Book;
 import kr.ac.yeonsung.demo.domain.club.Club;
 import kr.ac.yeonsung.demo.service.ClubService;
-import kr.ac.yeonsung.demo.service.JoinClubService;
-import kr.ac.yeonsung.demo.service.JoinService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
@@ -21,46 +17,42 @@ import java.util.List;
 @Controller
 @RequiredArgsConstructor
 public class ClubController {
-    @Autowired
     private final ClubService clubService;
-    @Autowired
-    private final JoinService joinService;
-    @Autowired
-    private final JoinClubService joinClubService;
-
 
     @GetMapping("/clubs/new")
-    public String createForm(Model model) {
-        model.addAttribute("form", new BookForm());
-        return "clubs/createClubForm";
+    public String createForm(Model model){
+       model.addAttribute("form",new BookForm());
+       return "clubs/createClubForm";
     }
 
     @PostMapping("/clubs/new")
-    public String create(BookForm form) {
+    public String create(BookForm form){
         Book book = new Book();
         book.setName(form.getName());
         book.setTotalNumber(form.getTotalNumber());
         book.setAuthor(form.getAuthor());
         book.setIsbn(form.getIsbn());
+
         clubService.saveClub(book);
 
         return "redirect:/";
     }
 
     @GetMapping("/clubs")
-    public String list(Model model) {
+    public String list(Model model){
         List<Club> clubs = clubService.findClub();
-        model.addAttribute("clubs", clubs);
+        model.addAttribute("clubs",clubs);
         return "clubs/clubList";
     }
 
-    @PostMapping("clubs/{clubId}/delete")
-    public String deleteClub(@PathVariable("clubId") Long clubId){
-        Club club = clubService.findOne(clubId);
-        clubService.deleteClub(club);
-        return "redirect:/clubs";
     }
 
+
+    @PostMapping("clubs/{clubId}/delete")
+    public String deleteClub(@PathVariable("clubId") Long clubId){
+        clubService.deleteClub(clubId);
+        return "redirect:/clubs";
+    }
 
     @GetMapping("/clubs/{clubId}/change")
     public String changeForm(Model model,@PathVariable("clubId") Long clubId){
@@ -72,7 +64,7 @@ public class ClubController {
     @PostMapping("/clubs/{clubId}/change")
     public String change(BookForm form,@PathVariable("clubId") Long clubId){
         clubService.updateClub(clubId,form.getName(),form.getTotalNumber(),
-                form.getAuthor(),form.getIsbn());
+                                form.getAuthor(),form.getIsbn());
         return "redirect:/clubs";
     }
 }
