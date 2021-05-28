19c0d59d5eb6cf7daa98b3f07a7b6f2d26dae00a
LEESANGNAM <fgh0116@naver.com>
2021-05-29 00:36:01 +0900

    13주(vaild완료,테스트코드완료)

--- src/test/java/kr/ac/yeonsung/demo/service/ClubServiceTest.java
+++ src/test/java/kr/ac/yeonsung/demo/service/ClubServiceTest.java
@@ -24,6 +24,7 @@ import javax.persistence.PersistenceContext;
 import java.util.regex.Pattern;
 
 import static junit.framework.TestCase.assertTrue;
+import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.*;
 
 
@@ -50,28 +51,28 @@ class ClubServiceTest {
     public void 클럽생성() throws Exception{
         //given
         Member member = createMember();
-        Book book =createBook("testClub",10, member.getName());
+        Club club = createClub("testClub",10, member.getName());
         //when
         em.flush();
-        Book book2= (Book) clubService.findOne(book.getId());
+        Club club2 = clubService.findOne(club.getId());
         //then
-        assertEquals(book, book2);
+        assertEquals(club, club2);
     }
 
     @Test
     public void 삭제() throws Exception{
         //given
         Member member = createMember();
-        Book book = createBook("TestName",10, member.getName());
+        Club club = createClub("TestName",10, member.getName());
 
         int joinCount = 1;
-        Long join = joinService.Join(member.getId(), book.getId(), joinCount);
+        Long join = joinService.Join(member.getId(), club.getId(), joinCount);
         Join getjoin = joinService.findOne(join);
         //when
-        joinClubService.chageStatus(book); // 멤버상태변경
+        joinClubService.chageStatus(club); // 멤버상태변경
         joinClubRepository.deleteById(1l);
-        clubService.deleteClub(book.getId()); // 동아리삭제
-        Club one = clubService.findOne(book.getId());// 동아리아이디로 찾기 (삭제되었으면 null)
+        clubService.deleteClub(club.getId()); // 동아리삭제
+        Club one = clubService.findOne(club.getId());// 동아리아이디로 찾기 (삭제되었으면 null)
 
         //then
 
@@ -85,7 +86,7 @@ class ClubServiceTest {
         //given
         Member member = createMember();
 
-        Club club = createBook("테스트2",10,member.getName());
+        Club club = createClub("테스트2",10,member.getName());
         //when
         clubService.findClub();
         //then
@@ -95,96 +96,96 @@ class ClubServiceTest {
     public void 수정() throws Exception{
         //given
         Member member = createMember();
-        Book book = createBook("동아리테스트",10,member.getName());
-        Club one = clubService.findOne(book.getId()); //생성하고 해당 객체를 찾아놓는다.
+        Club club  = createClub("동아리테스트",10,member.getName());
+        Club one = clubService.findOne(club.getId()); //생성하고 해당 객체를 찾아놓는다.
         //when
-        clubService.updateClub(book.getId(),"수정테스트동아리",10,"수정테스트 멤버이름"); //값을 변경한다.
+        clubService.updateClub(club.getId(),"수정테스트동아리",10,"수정테스트 멤버이름"); //값을 변경한다.
         //then
-        assertEquals("동아리 번호확인",one.getId(),book.getId()); //수정전 객체와 수정후 객체의 번호가 같은지 확인
-        assertEquals("동아리 이름수정확인","수정테스트동아리",book.getName());
-        assertEquals("동아리 가입인원 확인",10,book.getTotalNumber());
-        assertEquals("동아리 회장이름 확인","수정테스트 멤버이름",book.getClubJang());
+        assertEquals("동아리 번호확인",one.getId(),club.getId()); //수정전 객체와 수정후 객체의 번호가 같은지 확인
+        assertEquals("동아리 이름수정확인","수정테스트동아리",club.getName());
+        assertEquals("동아리 가입인원 확인",10,club.getTotalNumber());
+        assertEquals("동아리 회장이름 확인","수정테스트 멤버이름",club.getClubJang());
     }
 
      @Test
       public void 동아리명_null() throws Exception{
          //given
-         Book book = new Book();
+         Club club = new Club();
          Member member = createMember();
-         book.setClubJang(member.getName());
-         book.setTotalNumber(10);
-         clubService.saveClub(book);
+         club.setClubJang(member.getName());
+         club.setTotalNumber(10);
+         clubService.saveClub(club);
           //when
          Club one = clubService.findOne(41l);
 
          //then
-         assertEquals(book.getName(), one.getName());
+         assertEquals(club.getName(), one.getName());
          assertNull(one.getName());
 
      }
      @Test
       public void 동아리명_not_null() throws Exception{
          //given
-         Book book = new Book();
-         book.setName("testName");
-         clubService.saveClub(book);
+         Club club = new Club();
+         club.setName("testName");
+         clubService.saveClub(club);
          //when
          Club one = clubService.findOne(41l);
 
          //then
-         assertEquals(book.getName(), one.getName());
+         assertEquals(club.getName(), one.getName());
          assertNotNull(one.getName());
      }
       @Test
        public void 동아리명10초과() throws Exception{
           //given
-           Book book = new Book();
-           book.setName("일이삼사오육칠팔구십초과");
-           clubService.saveClub(book);
+          Club club = new Club();
+          club.setName("일이삼사오육칠팔구십초과");
+           clubService.saveClub(club);
            //when
           Club one = clubService.findOne(41l);
           int length = one.getName().length();
           //then
-          assertEquals(book.getName(), one.getName());
+          assertEquals(club.getName(), one.getName());
           assertTrue(length>10);
       }
       @Test
        public void 동아리명10이하() throws Exception{
           //given
-           Book book = new Book();
-           book.setName("일이삼사오육칠팔구");
-           clubService.saveClub(book);
+          Club club = new Club();
+          club.setName("일이삼사오육칠팔구");
+           clubService.saveClub(club);
            //when
           Club one = clubService.findOne(41l);
           int length = one.getName().length();
           //then
-          assertEquals(book.getName(), one.getName());
+          assertEquals(club.getName(), one.getName());
           assertTrue(length<10);
       }
        @Test
         public void 가입인원1미만() throws Exception{
            //given
-            Book book = new Book();
-            book.setTotalNumber(0);
-            clubService.saveClub(book);
+           Club club = new Club();
+           club.setTotalNumber(0);
+            clubService.saveClub(club);
             //when
            Club one = clubService.findOne(41l);
            int totalNumber = one.getTotalNumber();
            //then
-           assertEquals(book.getId(),one.getId());
+           assertEquals(club.getId(),one.getId());
            assertTrue(totalNumber<1);
        }
        @Test
         public void 가입인원20초과() throws Exception{
            //given
-            Book book = new Book();
-            book.setTotalNumber(21);
-            clubService.saveClub(book);
+           Club club = new Club();
+           club.setTotalNumber(21);
+            clubService.saveClub(club);
             //when
            Club one = clubService.findOne(41l);
            int totalNumber = one.getTotalNumber();
            //then
-           assertEquals(book.getId(),one.getId());
+           assertEquals(club.getId(),one.getId());
            assertTrue(totalNumber>20);
        }
        @Test
@@ -193,22 +194,44 @@ class ClubServiceTest {
            String pattern = "^[\\s]*$"; //공백만체크
 
             //given
-            Book book = new Book();
-            book.setName("    name");
-            clubService.saveClub(book);
+           Club club = new Club();
+           club.setName("    name");
+            clubService.saveClub(club);
             //when
            Club one = clubService.findOne(41l);
            //then
            assertFalse(Pattern.matches(pattern,one.getName()));//title이 공백만있는게 아니라면 True
        }
+       @Test
+        public void 동아리명특수문자제외() throws Exception{
+           //정규식
+           String pattern = "^[가-힣|a-z|A-Z|0-9|_ |]*$"; //특수문자
 
-    private Book createBook(String name,int totalCount,String memberName) {
-        Book book = new Book();
-        book.setName(name);
-        book.setTotalNumber(totalCount);
-        book.setClubJang(memberName);
-        em.persist(book);
-        return book;
+            //given
+            Club club = new Club();
+            club.setName("@테스트네임");
+            clubService.saveClub(club);
+            //when
+           Club one = clubService.findOne(41l);
+           //then
+           assertFalse(Pattern.matches(pattern,one.getName())); //name이 정규식에 일치하지않으면 통과
+           assertThat(pattern.matches("he!!o"), is(false)); //해당 문자열이 정규식에 일치하지않는다.
+           assertThat(pattern.matches("hell()"), is(false));
+           assertThat(pattern.matches("\"hello\""), is(false));
+           assertThat(pattern.matches("hello^^"), is(false));
+           assertThat(pattern.matches("<hello>"), is(false));
+           assertThat(pattern.matches("#hello$"), is(false));
+       }
+
+
+
+    private Club createClub(String name,int totalCount,String memberName) {
+        Club club = new Club();
+        club.setName(name);
+        club.setTotalNumber(totalCount);
+        club.setClubJang(memberName);
+        em.persist(club);
+        return club;
     }
 
     private Member createMember() {
